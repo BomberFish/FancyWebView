b04d131b7fca8ea96677e5afe913371e60d77908
BomberFish <bomberfish@bomberfish.ca>
2023-12-03 11:42:39 -0500

    Initial Commit

---
+++ fancywebview/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – fancywebview
+// created on 2023-12-03
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fancywebview/fancywebviewApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// fancywebviewApp.swift – fancywebview
+// created on 2023-12-03
+
+import SwiftUI
+
+@main
+struct fancywebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
